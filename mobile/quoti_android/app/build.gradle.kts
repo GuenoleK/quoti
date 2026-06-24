90c3cf82a68983854421f5818e4e2f07e5c3a3e1
GuenoleK <GuenoleK@users.noreply.github.com>
2026-06-24 02:02:36 +0200

    Android + Extension: prepare 0.1.2 development

--- mobile/quoti_android/app/build.gradle.kts
+++ mobile/quoti_android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.quoti.android"
         minSdk = 26
         targetSdk = 36
-        versionCode = 106
-        versionName = "0.1.1"
+        versionCode = 107
+        versionName = "0.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
